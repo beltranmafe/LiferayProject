4648df47b94d90283d4bdf74d87408a6fdd7aef8
Cristian Eduardo Puechambud Ladino <cristian.puerchambud@vasslatam.com>
2020-02-11 10:41:46 -0500

    Subiendo Modulo Test

---
+++ modules/TestModule/src/main/java/com/liferay/test/moduletest/constants/TestModulePortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.test.moduletest.constants;
+
+/**
+ * @author cristian.puerchambud
+ */
+public class TestModulePortletKeys {
+
+	public static final String TESTMODULE =
+		"com_liferay_test_moduletest_TestModulePortlet";
+
+}
---
+++ modules/TestModule/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,8 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="testmodule.caption"/></b>
+</p>
+
+<h1>Haciendo uan prueba con Git</h1>
+<h1>Modificacion  : c 10:35am</h1>
